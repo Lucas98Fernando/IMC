ec814e5c10ee7878b3e01b9ff5843e9114332e90
Lucas <lucas98fernando@gmail.com>
2020-09-13 18:37:55 -0300

    Visual changes

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   View,
   Text,
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
-  TouchableOpacity
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Image,
+  ImageBackground
 } from 'react-native';
 
 type Props = {};
@@ -44,10 +46,10 @@ export default class App extends Component<Props> {
       s.resultText = 'Obesidade grau 1'
     }
     else if (s.result < 40) {
-      s.resultText = 'Obesidade grau 2 (Severa)'
+      s.resultText = 'Obesidade grau 2'
     }
     else {
-      s.resultText = 'Obesidade grau 3 (Mórbida)'
+      s.resultText = 'Obesidade grau 3'
     }
     this.setState(s)
   } 
@@ -55,34 +57,50 @@ export default class App extends Component<Props> {
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.mainContainer}>
+        <KeyboardAvoidingView style={styles.mainContainer}>
+          <ImageBackground
+            style={styles.bgImage}
+            source={require('./img/bg_white.jpg')}>
+            <View>
+              <Text style={styles.title}>IMC</Text>
+              <Text style={styles.description}>(Índice de Massa Corpórea)</Text>
+              <Image 
+                style={styles.img}
+                source={require('./img/coracao.png')}/>
+              
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    placeholder={'Massa'}
+                    keyboardType={'numeric'}
+                    style={styles.inputFields}
+                    onChangeText={(massa)=>{this.setState({massa})}}
+                  />
+                  <TextInput
+                    placeholder={'Altura'}
+                    keyboardType={'numeric'}
+                    style={styles.inputFields}
+                    onChangeText={(altura)=>{this.setState({altura})}}
+                  />
+                </View>
 
-          <Text style={styles.title}>IMC</Text>
-          <Text style={styles.description}>(Índice de Massa Corpórea)</Text>
-          
-            <View style={styles.inputContainer}>
-              <TextInput
-                placeholder={'Massa'}
-                keyboardType={'numeric'}
-                style={styles.inputFields}
-                onChangeText={(massa)=>{this.setState({massa})}}
-              />
-              <TextInput
-                placeholder={'Altura'}
-                keyboardType={'numeric'}
-                style={styles.inputFields}
-                onChangeText={(altura)=>{this.setState({altura})}}
-              />
+                <TouchableOpacity 
+                  style={styles.btnCalculate} 
+                  onPress={this.calculate}
+                  onPressIn={Keyboard.dismiss}
+                >
+                  <Text style={styles.textBtn}>Calcular</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.result}>{this.state.result.toFixed(2)}</Text>
+                <Text style={[styles.result, {fontSize: 30, textAlign: 'center'}]}>{this.state.resultText}</Text>
+
+                <Text style={styles.footer}>
+                  Desenvolvido por: <Text style={{color: '#f10'}}>Lucas Fernando</Text>.
+                </Text>
+
             </View>
-
-            <TouchableOpacity style={styles.btnCalculate} onPress={this.calculate}>
-              <Text style={styles.textBtn}>Calcular</Text>
-            </TouchableOpacity>
-
-            <Text style={styles.result}>{this.state.result.toFixed(2)}</Text>
-            <Text style={[styles.result, {fontSize: 30, textAlign: 'center'}]}>{this.state.resultText}</Text>
-
-        </View>
+          </ImageBackground>
+        </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
     );
   }
@@ -101,14 +119,17 @@ const styles = StyleSheet.create({
     width: '45%',
     borderWidth: 1,
     borderStyle: 'solid',
+    margin: 5,
     fontSize: 25,
     textAlign: 'center',
+    borderRadius: 10,
   },
   btnCalculate: {
-    marginTop: 20,
+    marginTop: 15,
     backgroundColor: '#89ffa5',
     alignSelf: 'center',
-    width: '90%'
+    width: '94%',
+    borderRadius: 10,
   },
   textBtn: {
     alignSelf: 'center',
@@ -116,12 +137,13 @@ const styles = StyleSheet.create({
     padding: 10,
     height: 60,
     fontSize: 25,
+    fontWeight: '600',
   },
   result: {
     alignSelf: 'center',
-    color: 'lightgray',
+    color: 'gray',
     fontSize: 45,
-    paddingTop: 20,
+    paddingTop: 15,
   },
   title: {
     alignSelf: 'center',
@@ -132,6 +154,21 @@ const styles = StyleSheet.create({
   description: {
     alignSelf: 'center',
     fontSize: 20,
-    marginBottom: 30,
+    marginBottom: 10,
+  },
+  img: {
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
+  bgImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    opacity: 0.9,
+  },
+  footer: {
+    alignSelf: 'center',
+    marginTop: 130,
+    justifyContent: 'flex-end',
   },
 }); 
